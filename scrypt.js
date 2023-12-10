e755e4684d589747371b283e7df785141de14872
Viktor Disevic <rosolmaxim32@gmail.com>
2023-12-10 23:36:40 +0100

    Dodavanje preostalih fajlova

---
+++ scrypt.js
@@ -0,0 +1,12 @@
+document.getElementById('moreInfo').addEventListener('click', clickForMoreInfo);
+
+document.getElementById('moreInfo').addEventListener('click', function() {
+    window.location.href = 'https://bitcoin.org/bitcoin.pdf'; // Ovde stavite link ka stranici sa dodatnim informacijama
+});
+
+document.getElementById('videoShorts').addEventListener('click', clickForMoreInfo);
+
+document.getElementById('videoShorts').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/shorts/WaHLIKyMOZQ'; // Ovde stavite link ka stranici sa dodatnim informacijama
+});
+
